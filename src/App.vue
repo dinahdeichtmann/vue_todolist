2add9bd897b0b88add794dbe52f75c95428da58b
Dinah Deichtmann <dinahdeichtmann@gmail.com>
2023-02-16 15:35:47 +0100

    greeting func: save name to / display name from local storage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, onMounted, ref, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const inputContent = ref("");
+const inputCategory = ref(null);
+
+const todosAscending = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(name, (newValue) => {
+  localStorage.setItem("name", newValue);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
