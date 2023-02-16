99cf1d58b654a71770d873924d13284793fc5401
Dinah Deichtmann <dinahdeichtmann@gmail.com>
2023-02-16 15:52:40 +0100

    to do section + form template

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ const todosAscending = computed(() =>
   })
 );
 
+function addTodo() {}
+
 watch(name, (newValue) => {
   localStorage.setItem("name", newValue);
 });
@@ -28,6 +30,41 @@ onMounted(() => {
       <h2 class="title">
         What's up, <input type="text" placeholder="Name here" v-model="name" />
       </h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="inputContent"
+        />
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="inputCategory"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="inputCategory"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo" />
+      </form>
     </section>
   </main>
 </template>
